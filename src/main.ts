23ff7147b0b0e279569a99de9cca8f76b58131af
dioaryatama <dioaryatama110699@gmail.com>
2026-04-04 18:24:08 +0700

    update vercel

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express'; // Hanya sebagai adapter/jembatan
+import express from 'express';
 
-// 1. Buat instance express sebagai 'container'
 const server = express();
 
-export const createNestServer = async (expressInstance) => {
-  // 2. Masukkan expressInstance ke dalam NestJS lewat ExpressAdapter
-  const app = await NestFactory.create(
-    AppModule,
-    new ExpressAdapter(expressInstance),
-  );
+// ✅ cache instance
+let isInitialized = false;
 
-  // Semua config NestJS kamu tetap di sini
-  app.setGlobalPrefix('api/v2');
-  app.enableCors();
+const createNestServer = async () => {
+  if (!isInitialized) {
+    const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
-  // 3. PENTING: Gunakan init(), bukan listen()
-  await app.init();
+    app.setGlobalPrefix('api/v2');
+    app.enableCors();
+
+    await app.init();
+
+    isInitialized = true;
+  }
 };
 
-// 4. Export handler untuk dibaca oleh Vercel
 export default async (req: any, res: any) => {
-  await createNestServer(server);
-  server(req, res);
+  await createNestServer();
+  return server(req, res);
 };
