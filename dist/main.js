0fc8dd8f66fb15306401e9937fb33a5264fe28d4
dioaryatama <dioaryatama110699@gmail.com>
2026-04-17 14:01:09 +0700

    test

--- dist/main.js
+++ dist/main.js
@@ -1,31 +1,33 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
-const common_1 = require("@nestjs/common");
-const swagger_1 = require("@nestjs/swagger");
-const config_1 = require("@nestjs/config");
 const app_module_1 = require("./app.module");
-async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule, { logger: ['log', 'error', 'warn'] });
-    const config = app.get(config_1.ConfigService);
-    app.setGlobalPrefix('api/v2');
-    app.enableCors({ origin: config.get('app.corsOrigins'), credentials: true });
-    app.useGlobalPipes(new common_1.ValidationPipe({
-        whitelist: true, transform: true,
-        transformOptions: { enableImplicitConversion: true },
-    }));
-    const swaggerCfg = new swagger_1.DocumentBuilder()
-        .setTitle('SIMRS v2 API')
-        .setDescription('Sistem Informasi Manajemen Rumah Sakit — REST API lengkap')
-        .setVersion('2.0.0')
-        .addBearerAuth()
-        .addServer('http://localhost:3000', 'Development')
-        .build();
-    swagger_1.SwaggerModule.setup('api/docs', app, swagger_1.SwaggerModule.createDocument(app, swaggerCfg), { swaggerOptions: { persistAuthorization: true } });
-    const port = config.get('app.port') || 3000;
-    await app.listen(port);
-    console.log(`\n🏥 SIMRS v2 API  → http://localhost:${port}/api/v2`);
-    console.log(`📚 Swagger Docs  → http://localhost:${port}/api/docs\n`);
-}
-bootstrap();
+const platform_express_1 = require("@nestjs/platform-express");
+const express_1 = __importDefault(require("express"));
+const swagger_1 = require("@nestjs/swagger");
+const server = (0, express_1.default)();
+let isInitialized = false;
+const createNestServer = async () => {
+    if (!isInitialized) {
+        const app = await core_1.NestFactory.create(app_module_1.AppModule, new platform_express_1.ExpressAdapter(server));
+        app.setGlobalPrefix('api/v2');
+        app.enableCors();
+        const config = new swagger_1.DocumentBuilder()
+            .setTitle('SIMRS API')
+            .setDescription('API Documentation')
+            .setVersion('1.0')
+            .build();
+        const document = swagger_1.SwaggerModule.createDocument(app, config);
+        swagger_1.SwaggerModule.setup('docs', app, document);
+        await app.init();
+        isInitialized = true;
+    }
+};
+exports.default = async (req, res) => {
+    await createNestServer();
+    return server(req, res);
+};
 //# sourceMappingURL=main.js.map
